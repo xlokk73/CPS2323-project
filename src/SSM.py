11f0c2f614570617ab8cd48057cd1f9d73e3352e
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2020-12-22 11:16:38 +0100

    Created function for encryption

--- src/SSM.py
+++ src/SSM.py
@@ -1,46 +1,30 @@
-import keys
+from keys import *
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from Crypto.Protocol.KDF import PBKDF2
 
 
-application_keys = [keys.get_application_key(), keys.get_application_key()]
+application_keys = [get_application_key(), get_application_key()]
 
 
 ## Store encrypted Key Encryption Key
 
 output_file = 'file_vault.bin' # Output file
-data = keys.get_key_encryption_key() # Must be a bytes object
+open(output_file, 'w').close()
+
+data = get_key_encryption_key() # Must be a bytes object
 
 MK_1_pass = "master key 1"
-key = PBKDF2(MK_1_pass, keys.get_master_key(keys.get_master_key_part_1(), keys.get_master_key_part_2()), dkLen=16)
+key = PBKDF2(MK_1_pass, get_master_key(get_master_key_part_1(), get_master_key_part_2()), dkLen=16)
 
-
-# Create cipher object and encrypt the data
-cipher = AES.new(key, AES.MODE_CBC) # Create a AES cipher object with the key using the mode CBC
-ciphered_data = cipher.encrypt(pad(data, AES.block_size)) # Pad the input data and then encrypt
-
-
-file_out = open(output_file, "wb") # Open file to write bytes
-file_out.write(cipher.iv) # Write the iv to the output file (will be required for decryption)
-file_out.write(ciphered_data) # Write the varying length ciphertext to the file (this is the encrypted data)
-file_out.close()
+encrypt_and_store(data, key, output_file)
 
 
 ## Store encrypted Application Keys
 for application_key in application_keys:
-    output_file = 'file_vault.bin' # Output file
     data = application_key # Must be a bytes object
-    key = keys.get_key_encryption_key() # The key you generated
+    key = get_key_encryption_key() # The key you generated
 
-    # Create cipher object and encrypt the data
-    cipher = AES.new(key, AES.MODE_CBC) # Create a AES cipher object with the key using the mode CBC
-    ciphered_data = cipher.encrypt(pad(data, AES.block_size)) # Pad the input data and then encrypt
-
-    file_out = open(output_file, "ab") # Open file to append bytes
-    file_out.write(cipher.iv) # Write the iv to the output file (will be required for decryption)
-    file_out.write(ciphered_data) # Write the varying length ciphertext to the file (this is the encrypted data)
-    file_out.close()
-
+    encrypt_and_store(data, key, output_file)
 
--- src/keys.py
+++ src/keys.py
@@ -1,4 +1,6 @@
 from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
 def get_master_key_part_1():
     return get_random_bytes(8)
@@ -14,5 +16,16 @@ def get_application_key():
 
 def get_key_encryption_key():
     return get_random_bytes(16)
+
+def encrypt_and_store(data, key, output_file):
+    # Create cipher object and encrypt the data
+    cipher = AES.new(key, AES.MODE_CBC) # Create a AES cipher object with the key using the mode CBC
+    ciphered_data = cipher.encrypt(pad(data, AES.block_size)) # Pad the input data and then encrypt
+    
+    
+    file_out = open(output_file, "ab") # Open file to write bytes
+    file_out.write(cipher.iv) # Write the iv to the output file (will be required for decryption)
+    file_out.write(ciphered_data) # Write the varying length ciphertext to the file (this is the encrypted data)
+    file_out.close()
 
 
