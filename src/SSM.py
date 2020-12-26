9d8408d15d3d28fde4191c503b4775e7a6dea5b2
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2020-12-26 11:39:02 +0100

    Now writing Key encryption key MACed with MK_2

--- src/SSM.py
+++ src/SSM.py
@@ -1,28 +1,32 @@
+from Crypto.Protocol.KDF import PBKDF2, HKDF
+from Crypto.Hash import HMAC, SHA256, SHA512
 from keys import *
-from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-from Crypto.Protocol.KDF import PBKDF2
 
 application_keys = [get_application_key(), get_application_key()]
 
-## Store encrypted Key Encryption Key
+output_file = 'file_vault.bin'  # Output file
+open(output_file, 'w').close()  # Clear file contents
+KEK = b'keyencryptionkey'  # Must be a bytes object
 
-output_file = 'file_vault.bin' # Output file
-open(output_file, 'w').close()
-
-data = get_key_encryption_key() # Must be a bytes object
+MK_part_1 = b'jfklsdjl134jkl24'
+MK_part_2 = b'dsjfkldsjfkldsj4'
 
 MK_1_pass = "master key 1"
-MK_1 = PBKDF2(MK_1_pass, get_master_key(get_master_key_part_1(), get_master_key_part_2()), dkLen=16)
+MK_1 = PBKDF2(MK_1_pass, get_master_key(MK_part_1, MK_part_2), dkLen=16)
 
-encrypt_and_store(data, MK_1, output_file)
+# Store encrypted Key Encryption Key
+encrypt_and_store(MK_1, KEK, output_file)
 
+MK_2_pass = "master key 2"
+MK_2 = PBKDF2(MK_2_pass, get_master_key(MK_part_1, MK_part_2), dkLen=16)
 
-## Store encrypted Application Keys
+mac_and_store(MK_2, KEK, output_file)
+
+KEK_1_pass = "kek 1"
+KEK_1 = PBKDF2(KEK_1_pass, KEK, dkLen=16)
+
+# Store encrypted Application Keys
 for application_key in application_keys:
-    data = application_key # Must be a bytes object
-    key = get_key_encryption_key() # The key you generated
+    data = application_key  # Must be a bytes object
 
-    encrypt_and_store(data, key, output_file)
-
+    encrypt_and_store(KEK_1, data, output_file)
--- src/keys.py
+++ src/keys.py
@@ -1,30 +1,34 @@
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
+from Crypto.Hash import HMAC, SHA256, SHA512
 
-def get_master_key_part_1():
-    return get_random_bytes(16)
-
-def get_master_key_part_2():
-    return get_random_bytes(16)
 
 def get_master_key(master_key_part_1, master_key_part_2):
     # Returns XORed keys
     return bytes(a ^ b for (a, b) in zip(master_key_part_1, master_key_part_2))
 
+
 def get_application_key():
     return get_random_bytes(16)
 
-def get_key_encryption_key():
-    return get_random_bytes(16)
 
-def encrypt_and_store(data, key, output_file):
+def encrypt_and_store(key, data, output_file):
     # Create cipher object and encrypt the data
-    cipher = AES.new(key, AES.MODE_CBC) # Create a AES cipher object with the key using the mode CBC
-    ciphered_data = cipher.encrypt(pad(data, AES.block_size)) # Pad the input data and then encrypt
-    
-    
-    file_out = open(output_file, "ab") # Open file to write bytes
-    file_out.write(cipher.iv) # Write the iv to the output file (will be required for decryption)
-    file_out.write(ciphered_data) # Write the varying length ciphertext to the file (this is the encrypted data)
+    cipher = AES.new(key, AES.MODE_CBC)  # Create a AES cipher object with the key using the mode CBC
+    ciphered_data = cipher.encrypt(pad(data, AES.block_size))  # Pad the input data and then encrypt
+
+    file_out = open(output_file, "ab")  # Open file to write bytes
+    file_out.write(cipher.iv)  # Write the iv to the output file (will be required for decryption)
+    file_out.write(ciphered_data)  # Write the varying length ciphertext to the file (this is the encrypted data)
+    file_out.close()
+
+
+def mac_and_store(key, message, output_file):
+    h = HMAC.new(key, digestmod=SHA256)
+    h.update(message)
+    # print(h.hexdigest())
+
+    file_out = open(output_file, "ab")  # Open file to write bytes
+    file_out.write(h.hexdigest().encode())  # Write in bytes
     file_out.close()
