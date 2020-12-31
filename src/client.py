fa1a259ac389d7ce2bf664b29afbcc986d7b1d69
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2020-12-31 11:55:16 +0100

    Made tls server support multiple clients using threading

--- src/client.py
+++ src/client.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3           # This is client.py file    
+
 from socket import create_connection
 from ssl import SSLContext, PROTOCOL_TLS_CLIENT
 
@@ -11,9 +13,16 @@ context.load_verify_locations('cert.pem')
 client = create_connection((ip, port))
 tls = context.wrap_socket(client, server_hostname=hostname)
 print(f'Using {tls.version()}\n')
+
+
 tls.sendall(b'Hello, world')
 
 data = tls.recv(1024)
 print(f'Server says: {data}')
 
-tls.sendall(b'ok')
+while(1):
+    msg = input("Enter your message (: ")
+    tls.sendall(msg.encode())
+
+    data = tls.recv(1024)
+    print(f'Server says: {data}')
--- src/server.py
+++ src/server.py
@@ -1,6 +1,63 @@
+#!/usr/bin/python3           # This is server.py file    
+
 from socket import socket, AF_INET, SOCK_STREAM
 from ssl import SSLContext, PROTOCOL_TLS_SERVER
+from Crypto.Protocol.KDF import PBKDF2
+from keys import *
+import threading
 
+def on_new_client(connection, address):
+
+    data = connection.recv(1024)
+    print(f'Client {address} Says: {data}')
+    
+    connection.sendall(b"Hello, Welcome")
+    
+    while(1):
+        data = connection.recv(1024)
+        print(f'Client {address} Says: {data}')
+    
+        connection.sendall(b'OK')
+    connection.close()
+
+
+application_keys = [gen_application_key(), gen_application_key()]
+
+output_file = 'file_vault.bin'  # Output file
+open(output_file, 'w').close()  # Clear file contents
+KEK = get_key_encryption_key()  # Must be a bytes object
+
+MK_part_1 = get_master_key_part_1()
+MK_part_2 = get_master_key_part_2()
+
+MK_1_pass = "master key 1"
+MK_1 = PBKDF2(MK_1_pass, get_master_key(MK_part_1, MK_part_2), dkLen=16)
+
+# Encrypt and store the Key Encryption Key
+encrypt_and_store(MK_1, KEK, output_file)
+
+MK_2_pass = "master key 2"
+MK_2 = PBKDF2(MK_2_pass, get_master_key(MK_part_1, MK_part_2), dkLen=16)
+
+# MAC and store the Key Encryption Key
+mac_and_store(MK_2, KEK, output_file)
+
+KEK_1_pass = "kek 1"
+KEK_1 = PBKDF2(KEK_1_pass, KEK, dkLen=16)
+
+# Encrypt and store Application Keys
+for application_key in application_keys:
+    encrypt_and_store(KEK_1, application_key, output_file)
+
+
+KEK_2_pass = "kek 2"
+KEK_2 = PBKDF2(KEK_2_pass, KEK, dkLen=16)
+
+# MAC and store Application Keys
+for application_key in application_keys:
+    mac_and_store(KEK_2, application_key, output_file)
+
+# Communication with the client
 
 ip = '127.0.0.1'
 port = 8443
@@ -12,13 +69,12 @@ server = socket(AF_INET, SOCK_STREAM)
 server.bind((ip, port))
 server.listen(1)
 tls = context.wrap_socket(server, server_side=True)
-connection, address = tls.accept()
-print(f'Connected by {address}\n')
 
-data = connection.recv(1024)
-print(f'Client Says: {data}')
+while (1):
+    connection, address = tls.accept()
+    print(f'Connected by {address}\n')
 
-connection.sendall(b"You're welcome")
-
-data = connection.recv(1024)
-print(f'Client Says: {data}')
+    thread = threading.Thread(target=on_new_client,args=(connection,address))
+    thread.start()
+    
+tls.close()
